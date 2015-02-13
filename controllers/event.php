b6c0ca9bf2da4b4d184654cf3d1d1cf0dac74126
Dan Crack <dan@freethinkingdesign.co.uk>
2015-02-13 16:46:43 +0000

    started visit peterborough integration guzzle added

--- controllers/event.php
+++ controllers/event.php
@@ -2,6 +2,7 @@
 
 namespace Opencity;
 
+use GuzzleHttp\Client;
 use Silex\Application;
 use FastFeed\Factory as FFFactory;
 use Desarrolla2\Cache\Cache;
@@ -17,7 +18,38 @@ class Event
         $adapter->setOption('ttl', 300);
         $cache = new Cache($adapter);
 
-        if ( !$cache->has('feed') ) {
+        $allowedCategories = array(
+            "Dance", "Art", "Sport", "Music", "Theatre", "Event"
+        );
+
+
+        $switch = 'visitpeterborough';
+
+        if ( !$cache->has('feed') && $switch == 'visitpeterborough' ) {
+            $client = new Client();
+            $response = $client->get('http://webservices.thedms.co.uk/v4/dmswebservices.asmx/dmsGetEvents?Client1=demo&Client2=new%vision&AreaID=&Category=&EndDate=&Groups=&ItemType=&Name=&radius=&ReturnTop=&StartDate=&townId=');
+            $xml = $response->xml();
+
+            $feedItems = array();
+            foreach ($xml->ListOf->dmsEvent as $event) {
+//                $categories = $item->getTags();
+//                $filteredCats = array_values(array_intersect(array_values($categories), $allowedCategories));
+
+                $feedItem = array(
+                    'id' => uniqid(),
+                    'title' => (string)$event->ItemName,
+                    'date' =>(string) $event->StartDate, //  $item->getDate()->format('Y-m-d H:i:s'),
+                    'category' =>  '', // count($filteredCats) > 0 ? $filteredCats[0] : '',
+                    'description' => (string)$event->ItemType.' '.(string)$event->Description
+                );
+                $feedItems[] = $feedItem;
+            }
+            $cache->set('feed',$feedItems);
+        }
+
+
+
+        if ( !$cache->has('feed')  && $switch == 'rss' ) {
 
             $allowedCategories = array(
                 "Dance", "Art", "Sport", "Music", "Theatre", "Event"
@@ -28,6 +60,7 @@ class Event
             $fastFeed->addFeed('default', 'http://www.peterboroughtoday.co.uk/rss/cmlink/1.5073523');
             $fastFeed->addFeed('default', 'http://www.peterborough-cathedral.org.uk/feed_events.xml');
             $fastFeed->addFeed('default', 'http://www.peterborougharena.com/events-overview/feed/');
+
             $items = $fastFeed->fetch('default');
             $feedItems = array();
             foreach ($items as $item) {
@@ -39,7 +72,7 @@ class Event
                     'title' => $item->getName(),
                     'date' => $item->getDate()->format('Y-m-d H:i:s'),
                     'category' => count($filteredCats) > 0 ? $filteredCats[0] : '',
-                    'description' => $item->getIntro()
+                    'description' => $item->getContent()
                 );
                 $feedItems[] = $feedItem;
             }
