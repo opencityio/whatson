d339cc3ca729f5ebf75d3bff6706e1c9e5685f93
Dan Crack <dan@freethinkingdesign.co.uk>
2015-02-13 11:54:33 +0000

    additional cors options header

--- controllers/selfie.php
+++ controllers/selfie.php
@@ -50,7 +50,7 @@ class Selfie
 
         }
 
-        header('Access-Control-Allow-Origin: *');
+        //header('Access-Control-Allow-Origin: *');
         return $response;
     }
 }
--- web/index.php
+++ web/index.php
@@ -29,7 +29,7 @@ $app = new Silex\Application();
 $app->before(function (Request $request) {
     if ($request->getMethod() === "OPTIONS") {
         $response = new Response();
-        $response->headers->set("Access-Control-Allow-Origin",'*');
+        //$response->headers->set("Access-Control-Allow-Origin",'*');
         $response->headers->set("Access-Control-Allow-Methods","GET,POST,PUT,DELETE,OPTIONS");
         $response->headers->set("Access-Control-Allow-Headers","Content-Type");
         $response->setStatusCode(200);
@@ -39,7 +39,7 @@ $app->before(function (Request $request) {
 
 //handling CORS respons with right headers
 $app->after(function (Request $request, Response $response) {
-    $response->headers->set("Access-Control-Allow-Origin","*");
+    //$response->headers->set("Access-Control-Allow-Origin","*");
     $response->headers->set("Access-Control-Allow-Methods","GET,POST,PUT,DELETE,OPTIONS");
 });
 
